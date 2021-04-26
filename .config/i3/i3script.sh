cdf62c1ce74259f57ab29a66da582ff5f6f87ded
Chanasit <kai.chr160@gmail.com>
2021-04-26 15:22:11 +0700

    update i3 keyboard layout

--- .config/i3/i3script.sh
+++ .config/i3/i3script.sh
@@ -2,16 +2,4 @@
 
 # shell scipt to prepend i3status with more stuff
 
-i3status --config ~/.config/i3/i3status.conf | while :
-do
-    read line
-    LG=$(setxkbmap -query | awk '/layout/{print $2}')
-
-    if [ $LG == "us" ]
-    then
-        dat="[{ \"full_text\": \"LANG: $LG\", \"color\":\"#FFFFFF\" },"
-    else
-        dat="[{ \"full_text\": \"LANG: $LG\", \"color\":\"#FFFFFF\" },"
-    fi
-    echo "${line/[/$dat}" || exit 1
-done
+i3status --config ~/.config/i3/i3status.conf
